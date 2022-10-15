dd9d931f21fe87d9037fc52e4dcf13c87d8da6c3
Jaswanth Madiya <102235533+jaswanth070@users.noreply.github.com>
2022-10-15 18:47:52 +0530

    Update urls.py

--- Library/urls.py
+++ Library/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
-from django.views.static import static
+# from django.views.static import static
 from django.views.static import serve
-from django.conf.urls.static import url
+# from django.conf.urls.static import url
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app1.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+#     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
