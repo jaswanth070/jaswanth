844d5d134478101e98f2e87cf8a7d7401b3bdd84
Jaswanth Madiya <102235533+jaswanth070@users.noreply.github.com>
2022-10-15 16:21:44 +0530

    Add files via upload

---
+++ Library/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'elibrarydjango@gmail.com'
+EMAIL_HOST_PASSWORD = 'bzgqynehziicxdbn'
+EMAIL_PORT = 587
---
+++ app1/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length =200)
+    author = models.CharField(max_length =200)
+    isbn = models.PositiveIntegerField()
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name + "["+str(self.isbn)+']'
+
+class Contact(models.Model):
+    name = models.CharField(max_length = 122)
+    email = models.CharField(max_length = 122)
+    phone = models.CharField(max_length = 12)
+    desc = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    user = models.OneToOneField(User,on_delete=models. CASCADE)
+    grade = models.CharField(max_length=10)
+    branch = models.CharField(max_length=10,blank=True)
+    roll_no = models.CharField(max_length=3,blank=True)
+    phone = models.CharField(max_length=10,blank=True)
+
+    def __str__(self):
+        return str(self.user) + " ["+str(self.branch)+']'+ " ["+str(self.classroom)+']' + " ["+str(selfroll_no)+']'
