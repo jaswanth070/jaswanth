5b074d5b7667505c287a0915cb5f45c7cdd6a360
Jaswanth Madiya <102235533+jaswanth070@users.noreply.github.com>
2022-10-16 18:40:15 +0530

    Update urls.py

--- Library/urls.py
+++ Library/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from django.contrib.auth import views as auth_views
 # from django.views.static import static
 from django.views.static import serve
-from django.conf.urls.static import static
+# from django.conf.urls.static import static
 
 
 
