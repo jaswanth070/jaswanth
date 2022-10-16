0a57b2413c8b656488d75f3e2b55d6679692ad5f
Jaswanth Madiya <102235533+jaswanth070@users.noreply.github.com>
2022-10-16 19:00:04 +0530

    Update urls.py

--- Library/urls.py
+++ Library/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.contrib.auth import views as auth_views
 # from django.views.static import static
 from django.views.static import serve
