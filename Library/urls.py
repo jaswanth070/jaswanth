8acc6690a8d2480e6761b888602f2fa03477c890
Jaswanth Madiya <102235533+jaswanth070@users.noreply.github.com>
2022-10-16 18:27:16 +0530

    Update urls.py

--- Library/urls.py
+++ Library/urls.py
@@ -18,13 +18,18 @@ from django.urls import path,include
 from django.contrib.auth import views as auth_views
 # from django.views.static import static
 from django.views.static import serve
-# from django.conf.urls.static import url
+from django.conf.urls.static import static
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app1.urls')),
+    re_path((r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
 #     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
 #     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATICFILES_DIRS[0])
+
