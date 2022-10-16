3eaf9eba0494fddc1a01a8c3ed8121511dfc397a
Jaswanth Madiya <102235533+jaswanth070@users.noreply.github.com>
2022-10-16 18:42:06 +0530

    Update urls.py

--- Library/urls.py
+++ Library/urls.py
@@ -25,8 +25,8 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app1.urls')),
-    re_path((r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    re_path((r'^media/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path((r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    path((r'^media/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 #     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
 #     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
